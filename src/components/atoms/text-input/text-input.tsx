c6390fe31b0694d2b4880eaded896a6c9ef50ec4
Josh Farrant <josh@farrant.me>
2021-07-09 14:30:45 +0100

    Added tailwind

--- src/components/atoms/text-input/text-input.tsx
+++ src/components/atoms/text-input/text-input.tsx
@@ -4,5 +4,5 @@ import { TInputProperties } from './text-input.types';
 export const TextInput = (props: TInputProperties) => {
     const { id } = useInputGroup();
 
-    return <input id={id} {...props} />;
+    return <input className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md" id={id} {...props} />;
 };
---
+++ tailwind.config.js
@@ -0,0 +1,59 @@
+module.exports = {
+  purge: ['./src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+//   theme: {
+//       fontFamily: {
+//           sans: 'Lato, Arial, sans-serif',
+//           display: 'Montserrat, Arial, sans-serif',
+//       },
+//       fontSize: {
+//           sm: '14px',
+//           base: '16px',
+//       },
+//       fontWeight: {
+//           normal: 400,
+//           medium: 500,
+//           semiBold: 600,
+//           bold: 700,
+//       },
+//       colors: {
+//           purple: '#8A2D87',
+//           darkPurple: '#400F4C',
+//           indigo: '#282637',
+//           green: '#16835B',
+//           red: '#AA1D42',
+//           pink: '#DA285A',
+//           orange: '#C05119',
+//           teal: '#138081',
+//           yellow: '#FAC800',
+//           grey: {
+//               1: '#F9F9F9',
+//               2: '#EBEBEB',
+//               3: '#DBDBDB',
+//               4: '#BFBFBF',
+//               5: '#909090',
+//               6: '#686666',
+//               7: '#3C3B3B',
+//               8: '#3C3B3B',
+//           },
+//           white: '#FFFFFF',
+//           black: '#000000',
+//       },
+//       spacing: {
+//           0.5: '0.2rem',
+//           1: '0.4rem',
+//           2: '0.8rem',
+//           3: '1.2rem',
+//           4: '1.6rem',
+//           5: '2.0rem',
+//           6: '2.4rem',
+//           8: '3.2rem',
+//           12: '4.8rem',
+//           18: '7.2rem',
+//       },
+//   },
+  variants: {
+      extend: {},
+  },
+  plugins: [],
+};
